002d1071f8d1c183ce7a147501aab2b0a81e0e44
giuliamari04 <giuliamari04@gmail.com>
2024-01-08 16:59:16 +0100

    correzione index

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -15,7 +15,7 @@ class ComicController extends Controller
     public function index()
     {
         $comics = Comic::all();
-        return view('home', compact('comics'));
+        return view('comics.index', compact('comics'));
     }
 
     /**
